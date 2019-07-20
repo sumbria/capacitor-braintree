ffff7829967f1090d7c3783c462accb590d1a4bf
Balbinder Sumbria <sumbria.balbinder@gmail.com>
2019-07-20 04:32:05 -0500

    Pod path fix

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,44 +1,60 @@
-declare global {
-  interface PluginRegistry {
-    BraintreePlugin?: BraintreePlugin;
-  }
+import {Plugins} from "@capacitor/core";
 
+declare global {
+    interface PluginRegistry {
+        BTPlugin?: BraintreePlugin;
+    }
 }
 
-interface UIResult {
-  cancelled: boolean;
-  nonce: string;
-  type: string;
-  localizedDescription: string;
-  card: {
-      lastTwo: string;
-      network: string;
-  };
-  payPalAccount: {
-      email: string;
-      firstName: string;
-      lastName: string;
-      phone: string;
-      billingAddress: string;
-      shippingAddress: string;
-      clientMetadataId: string;
-      payerId: string;
-  };
-  applePaycard: {
-  };
-  threeDSecureCard: {
-      liabilityShifted: boolean;
-      liabilityShiftPossible: boolean;
-  };
-  venmoAccount: {
-      username: string;
-  };
+export interface UIResult {
+    cancelled: boolean;
+    nonce: string;
+    type: string;
+    localizedDescription: string;
+    card: {
+        lastTwo: string;
+        network: string;
+    };
+    payPalAccount: {
+        email: string;
+        firstName: string;
+        lastName: string;
+        phone: string;
+        billingAddress: string;
+        shippingAddress: string;
+        clientMetadataId: string;
+        payerId: string;
+    };
+    applePaycard: {};
+    threeDSecureCard: {
+        liabilityShifted: boolean;
+        liabilityShiftPossible: boolean;
+    };
+    venmoAccount: {
+        username: string;
+    };
 }
 
 export interface BraintreePlugin {
-  setToken(options: { token: string }): Promise<any>;
-  showDropIn(options: {
-    amount: string;
-    disabled?: string[];
-  }): Promise<UIResult>
+    setToken(options: { token: string }): Promise<any>;
+
+    showDropIn(options: {
+        amount: string;
+        disabled?: string[];
+    }): Promise<UIResult>
+}
+
+const {BTPlugin} = Plugins;
+
+export class Braintree implements BraintreePlugin {
+    setToken(options: { token: string }): Promise<any> {
+        return BTPlugin.setToken(options);
+    }
+
+    showDropIn(options: {
+        amount: string;
+        disabled?: string[];
+    }): Promise<UIResult> {
+        return BTPlugin.showDropIn(options);
+    }
 }
